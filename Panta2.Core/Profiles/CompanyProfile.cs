101c21d645b91a9fcc6b01a547964113264abdae
Simon Damiaens <simon.damiaens@student.pxl.be>
2023-03-17 00:50:35 +0100

    Add AutoMapper

---
+++ Panta2.Core/Profiles/CompanyProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Panta2.Core.Profiles
+{
+    public class CompanyProfile : Profile
+    {
+        public CompanyProfile()
+        {
+            CreateMap<Entities.Company, Models.CompanyDto>();
+        }
+    }
+}
