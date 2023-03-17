36ef1b3d82fef51f49f907b3643e630ea4af01fa
Simon Damiaens <simon.damiaens@student.pxl.be>
2023-03-17 15:43:53 +0100

    Fix Company TabelName issue

--- Panta2.Infrastructure/Context/DapperContext.cs
+++ Panta2.Infrastructure/Context/DapperContext.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Dapper.Contrib.Extensions;
+using Dapper;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Data;
+using Dapper.Mapper;
+using Panta2.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Panta2.Infrastructure.Context
 {
@@ -13,6 +18,19 @@ namespace Panta2.Infrastructure.Context
         {
             _configuration = configuration;
             _connectionString = _configuration.GetConnectionString("SqlConnection");
+
+            SqlMapperExtensions.TableNameMapper = entityType =>
+            {
+                if (entityType == typeof(Company))
+                {
+                    return "companies";
+                }
+                else if (entityType == typeof(Service))
+                {
+                    return "services";
+                }
+                throw new Exception($"Not supported entity type {entityType}");
+            };
         }
 
         public IDbConnection CreateConnection()
