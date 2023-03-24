5303dec3714c59bf59df85f7d476edd86253562d
Simon Damiaens <simon.damiaens@student.pxl.be>
2023-03-24 11:38:33 +0100

    Add loaing screen to login

--- Panta2.Frontend/src/app/login/login.component.ts
+++ Panta2.Frontend/src/app/login/login.component.ts
@@ -13,37 +13,36 @@ export class LoginComponent implements OnInit {
     username: null,
     password: null,
   };
-  isLoggedIn = false;
+  isLoggingIn = false;
   isLoginFailed = false;
   errorMessage = '';
-  roles: string[] = [];
 
   constructor(private authService: AuthService, private storageService: StorageService, private router: Router) { }
 
 
   ngOnInit(): void {
     if (this.storageService.isLoggedIn()) {
-      this.isLoggedIn = true;
       this.router.navigate(["/home"])
     }
   }
 
   onSubmit(): void {
     const { username, password } = this.form
-
+    this.isLoggingIn = true;
     this.authService.login(username, password).subscribe({
       next: data => {
         this.storageService.saveToken(data.token);
         this.storageService.saveId(data.id)
         this.isLoginFailed = false;
-        this.isLoggedIn = true;
         this.router.navigate(["/home"])
       },
       error: err => {
         if (err.status == 401) {
+          this.isLoggingIn = false;
           this.errorMessage = "Username or Password is not correct."
         }
         else {
+          this.isLoggingIn = false;
           this.errorMessage = "Something went wrong."
         }
         this.isLoginFailed = true;
