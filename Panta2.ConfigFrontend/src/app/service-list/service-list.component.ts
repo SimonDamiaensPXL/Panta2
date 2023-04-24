11e58c138787efcf2d822b22ba70d53c6dd74667
Simon Damiaens <simon.damiaens@student.pxl.be>
2023-04-24 09:40:44 +0200

    Add service list and refactor service overview

---
+++ Panta2.ConfigFrontend/src/app/service-list/service-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { Service } from '../core/models/service.model';
+import { ServiceService } from '../core/services/service/service.service';
+
+@Component({
+  selector: 'app-service-list',
+  templateUrl: './service-list.component.html',
+  styleUrls: ['./service-list.component.sass']
+})
+export class ServiceListComponent {
+  services: Service[] = [];
+  isLoading: boolean = true;
+
+  constructor(private serviceService: ServiceService) { }
+
+  async ngOnInit(): Promise<void> {
+    this.services = await firstValueFrom(this.serviceService.getServices());
+    this.isLoading = false;
+  }
+}
--- Panta2.ConfigFrontend/src/app/services-overview/services-overview.component.ts
+++ Panta2.ConfigFrontend/src/app/services-overview/services-overview.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Service } from '../core/models/service.model';
-import { ServiceService } from '../core/services/service/service.service';
-import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-services-overview',
@@ -9,13 +6,5 @@ import { firstValueFrom } from 'rxjs';
   styleUrls: ['./services-overview.component.sass']
 })
 export class ServicesOverviewComponent {
-  services: Service[] = [];
-  isLoading: boolean = true;
 
-  constructor(private serviceService: ServiceService) { }
-
-  async ngOnInit(): Promise<void> {
-    this.services = await firstValueFrom(this.serviceService.getServices());
-    this.isLoading = false;
-  }
 }
