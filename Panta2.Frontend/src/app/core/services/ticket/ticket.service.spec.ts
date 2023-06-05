8332a8fd0adb7a566f3f14334841bb95738779f0
Simon Damiaens <simon.damiaens@student.pxl.be>
2023-06-05 11:02:09 +0200

    Add ticket service Jest tests

--- Panta2.Frontend/src/app/core/services/ticket/ticket.service.spec.ts
+++ Panta2.Frontend/src/app/core/services/ticket/ticket.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from '../api/api.service';
 import { TicketService } from './ticket.service';
+import { Observable, of } from 'rxjs';
 
 describe('TicketService', () => {
   let ticketService: TicketService;
@@ -14,11 +15,67 @@ describe('TicketService', () => {
     ticketService = TestBed.inject(TicketService);
 
     apiServiceMock = {
-      put: jest.fn(),
-      get: jest.fn(),
+      get: jest.fn()
     }
 
     ticketService = new TicketService(apiServiceMock as ApiService);
   });
 
+  it('should be created', () => {
+    expect(ticketService).toBeTruthy();
+  });
+
+
+  describe('getTickets', () => {
+    it('should call the apiService get method with the correct endpoint and return the result', () => {
+      const mockResponse = [
+        { ticketNum: 12345, subject: 'Ticket 1', priority: 'Normal', state: 'Open', creationDate: new Date(), lastModificationDate: new Date() }
+      ];
+      (apiServiceMock.get as jest.Mock).mockReturnValue(of(mockResponse));
+
+      const result = ticketService.getTickets();
+
+      expect(apiServiceMock.get).toHaveBeenCalledWith('/tickets');
+      expect(result).toBeInstanceOf(Observable);
+      result.subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+    });
+  });
+
+  
+  describe('getTicketsCountedPerState', () => {
+    it('should call the apiService get method with the correct endpoint and return the result', () => {
+      const mockResponse = { openTicketCount: 4, inProgressTicketCount: 3, closedTicketCount: 3 };
+
+      (apiServiceMock.get as jest.Mock).mockReturnValue(of(mockResponse));
+
+      const result = ticketService.getTicketsCountedPerState();
+
+      expect(apiServiceMock.get).toHaveBeenCalledWith('/tickets/state-counted');
+      expect(result).toBeInstanceOf(Observable);
+      result.subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+    });
+  });
+
+  
+  describe('getTicketsCountedPerPriority', () => {
+    it('should call the apiService get method with the correct endpoint and return the result', () => {
+      const mockResponse = { normalTicketCount: 5, highTicketCount: 3, emergencyTicketCount: 1 };
+
+      (apiServiceMock.get as jest.Mock).mockReturnValue(of(mockResponse));
+
+      const result = ticketService.getTicketsCountedPerPriority();
+
+      expect(apiServiceMock.get).toHaveBeenCalledWith('/tickets/priority-counted');
+      expect(result).toBeInstanceOf(Observable);
+      result.subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+    });
+  });
 });
+
+
--- Panta2.Frontend/src/app/dashboard/dashboard.component.spec.ts
+++ Panta2.Frontend/src/app/dashboard/dashboard.component.spec.ts
@@ -5,93 +5,114 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HttpClientModule } from '@angular/common/http';
 import { of, throwError } from 'rxjs';
 import { expect } from '@jest/globals';
+import { TicketService } from "../core/services/ticket/ticket.service";
 
 describe('DashboardComponent', () => {
-    let component: DashboardComponent;
-    let fixture: ComponentFixture<DashboardComponent>;
-    let userService: UserService;
-    let storageService: StorageService;
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+  let userService: UserService;
+  let storageService: StorageService;
+  let ticketService: TicketService;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-          declarations: [ DashboardComponent ],
-          imports: [ HttpClientModule ],
-          providers: [
-            UserService,
-            StorageService
-          ],
-        })
-        .compileComponents();
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DashboardComponent],
+      imports: [HttpClientModule],
+      providers: [
+        UserService,
+        StorageService,
+        TicketService
+      ],
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
+    storageService = TestBed.inject(StorageService);
+    ticketService = TestBed.inject(TicketService);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize component properties correctly', () => {
+    expect(component.services).toEqual([]);
+    expect(component.favoriteServices).toEqual([]);
+    expect(component.filteredServices).toEqual([]);
+    expect(component.userName).toBeUndefined();
+    expect(component.companyLogo).toBeUndefined();
+    expect(component.isLoading).toBe(true);
+  });
+
+  describe('ngOnInit', () => {
+    const mockUser = { id: 'testUserId' };
+    const mockServices = [
+      { serviceId: 1, name: 'Service 1', icon: 'icon 1', link: 'link 1', isFavorite: true },
+      { serviceId: 2, name: 'Service 2', icon: 'icon 2', link: 'link 2', isFavorite: false }
+    ];
+    const mockFavoriteServices = [
+      { serviceId: 1, name: 'Service 1', icon: 'icon 1', link: 'link 1', isFavorite: true }
+    ];
+
+    const mockTickets = [
+      { ticketNum: 12345, subject: 'Ticket 1', priority: 'Normal', state: 'Open', creationDate: new Date(), lastModificationDate: new Date() }
+    ];
+
+    const mockCountedPerState = { openTicketCount: 4, inProgressTicketCount: 3, closedTicketCount: 3 }
+    const mockCountedPerPriority = { normalTicketCount: 5, highTicketCount: 3, emergencyTicketCount: 1 }
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(DashboardComponent);
-        component = fixture.componentInstance;
-        userService = TestBed.inject(UserService);
-        storageService = TestBed.inject(StorageService);
+      jest.spyOn(storageService, 'getUser').mockResolvedValue(mockUser);
+      jest.spyOn(userService, 'getServices').mockReturnValue(of(mockServices));
+      jest.spyOn(userService, 'getFavoriteServices').mockReturnValue(of(mockFavoriteServices));
+      jest.spyOn(ticketService, 'getTickets').mockReturnValue(of(mockTickets));
+      jest.spyOn(ticketService, 'getTicketsCountedPerState').mockReturnValue(of(mockCountedPerState));
+      jest.spyOn(ticketService, 'getTicketsCountedPerPriority').mockReturnValue(of(mockCountedPerPriority));
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should fetch user and services data and update component properties', async () => {
+      await component.ngOnInit();
+      expect(storageService.getUser).toHaveBeenCalled();
+      expect(userService.getServices).toHaveBeenCalledWith(mockUser.id);
+      expect(userService.getFavoriteServices).toHaveBeenCalledWith(mockUser.id);
+      expect(component.services).toEqual(mockServices);
+      expect(component.favoriteServices).toEqual(mockFavoriteServices);
+      expect(component.filteredServices).toEqual(mockServices);
+      expect(component.isLoading).toBe(false);
     });
 
-    it('should initialize component properties correctly', () => {
-        expect(component.services).toEqual([]);
-        expect(component.favoriteServices).toEqual([]);
-        expect(component.filteredServices).toEqual([]);
-        expect(component.userName).toBeUndefined();
-        expect(component.companyLogo).toBeUndefined();
-        expect(component.isLoading).toBe(true);
+    it('should handle errors and delete user storage', async () => {
+      const mockError = new Error('Test error');
+      jest.spyOn(console, 'error').mockImplementation(() => { }); // Mock console.error
+      jest.spyOn(storageService, 'deleteUserStorage').mockImplementation(() => { }); // Mock storageService.deleteUserStorage
+      jest.spyOn(userService, 'getServices').mockReturnValue(throwError(mockError));
+
+      await component.ngOnInit();
+
+      expect(console.error).toHaveBeenCalledWith(mockError);
+      expect(storageService.deleteUserStorage).toHaveBeenCalled();
     });
+  });
 
-    describe('ngOnInit', () => {
-        const mockUser = { id: 'testUserId' };
-        const mockServices = [
-            { serviceId: 1, name: 'Service 1', icon: 'icon 1', link: 'link 1', isFavorite: true },
-            { serviceId: 2, name: 'Service 2', icon: 'icon 2', link: 'link 2', isFavorite: false }
-        ];                      
-        const mockFavoriteServices = [
-            {serviceId: 1, name: 'Service 1', icon: 'icon 1', link: 'link 1', isFavorite: true }
-        ];
-    
-        beforeEach(() => {
-          jest.spyOn(storageService, 'getUser').mockResolvedValue(mockUser);
-          jest.spyOn(userService, 'getServices').mockReturnValue(of(mockServices));
-          jest.spyOn(userService, 'getFavoriteServices').mockReturnValue(of(mockFavoriteServices));
-        });
-    
-        it('should fetch user and services data and update component properties', async () => {
-          await component.ngOnInit();
-          expect(storageService.getUser).toHaveBeenCalled();
-          expect(userService.getServices).toHaveBeenCalledWith(mockUser.id);
-          expect(userService.getFavoriteServices).toHaveBeenCalledWith(mockUser.id);
-          expect(component.services).toEqual(mockServices);
-          expect(component.favoriteServices).toEqual(mockFavoriteServices);
-          expect(component.filteredServices).toEqual(mockServices);
-          expect(component.isLoading).toBe(false);
-        });
-
-        it('should handle errors and delete user storage', async () => {
-            const mockError = new Error('Test error');
-            jest.spyOn(console, 'error').mockImplementation(() => {}); // Mock console.error
-            jest.spyOn(storageService, 'deleteUserStorage').mockImplementation(() => {}); // Mock storageService.deleteUserStorage
-            jest.spyOn(userService, 'getServices').mockReturnValue(throwError(mockError));
-      
-            await component.ngOnInit();
-      
-            expect(console.error).toHaveBeenCalledWith(mockError);
-            expect(storageService.deleteUserStorage).toHaveBeenCalled();
-          });
+  describe('onFiltered', () => {
+    it('should update filteredServices with the provided array', () => {
+      const mockFilteredItems = [
+        { serviceId: 1, name: 'Service 1', icon: 'icon 1', link: 'link 1', isFavorite: true },
+        { serviceId: 2, name: 'Service 2', icon: 'icon 2', link: 'link 2', isFavorite: false }
+      ];
+      component.onFiltered(mockFilteredItems);
+      expect(component.filteredServices).toEqual(mockFilteredItems);
     });
+  });
 
-    describe('onFiltered', () => {
-        it('should update filteredServices with the provided array', () => {
-          const mockFilteredItems = [
-            { serviceId: 1, name: 'Service 1', icon: 'icon 1', link: 'link 1', isFavorite: true },
-            { serviceId: 2, name: 'Service 2', icon: 'icon 2', link: 'link 2', isFavorite: false }
-          ];
-          component.onFiltered(mockFilteredItems);
-          expect(component.filteredServices).toEqual(mockFilteredItems);
-        });
-      });
+  describe('getDateFormatted', () => {
+    it('should return formatted date', () => {
+      var date = new Date("2019-01-16");
+      expect(component.getDateFormatted(date)).toEqual("16-01-2019");
+    });
+  });
 });
